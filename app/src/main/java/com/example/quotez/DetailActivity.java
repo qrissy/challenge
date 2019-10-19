6534d3747cfdb2076eef28287ddd71b06f01d681
qris <qris09@gmail.com>
2019-10-19 17:35:52 +0100

    add animation to quote detail

---
+++ app/src/main/java/com/example/quotez/DetailActivity.java
@@ -0,0 +1,57 @@
+package com.example.quotez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+
+public class DetailActivity extends AppCompatActivity {
+    ImageButton Next;
+    ImageButton Prev;
+    TextView display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Next = (ImageButton) findViewById(R.id.imageBtNext);
+        Prev = (ImageButton) findViewById(R.id.imageBtPrev);
+        display = (TextView) findViewById(R.id.text);
+
+
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailActivity.this, NextDetailActivity.class));
+                //Intent i = new Intent(DetailActivity.this, NextDetailActivity.class);
+                //startActivity(i);
+                Animatoo.animateZoom(DetailActivity.this);
+            }
+        });
+
+
+        Prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailActivity.this, NextDetailActivity.class));
+                //Intent i = new Intent(DetailActivity.this, NextDetailActivity.class);
+                //startActivity(i);
+                Animatoo.animateSplit(DetailActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
+
